17315908dfe15ae3f9e0bacb9e9afcaf1fa295a3
CauchyUwU <lisa.scheipl1@gmail.com>
2020-09-04 13:15:58 +0200

    files added

---
+++ src/com/company/ScannerHandler.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class ScannerHandler { //a class that contains all the scanner data e.g. name path log path alt log path connection state (called when a new scanner gets connected)
+    //if the scanner is chosen from the scanner list but not connected set connection state false and variables null
+}
